e83ecbe908f0782c1ec338926eed95c9306a63c6
Stephanie H. Chang <stephanie@khanacademy.org>
2012-06-19 17:00:56 -0700

    add versioning and v2 layout

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Lesson Prototype
 
-## Overview
-
-Main project files/directories:
-- `index.html`
-- `lesson.js`
+## File organization:
+- `v1.html`
+- `v1.js`
+- `less/bootstrap-v1.less`
+- `less/v1.less`
 - `lessons/`: data and files for lessons
+
+## Screenshots of works-in-progress:
+
+### v0
+![v0](https://s3.amazonaws.com/uploads.hipchat.com/6574/98377/j9qhbud4fx42ham/v0.png)
+
+### v1
+![v1](https://s3.amazonaws.com/uploads.hipchat.com/6574/98377/1d0a7mour65e42d/upload.png)
+
+### v2
+![v2](https://s3.amazonaws.com/uploads.hipchat.com/6574/98377/zbs3v2iw770c3r7/upload.png)
---
+++ v2.js
@@ -0,0 +1,130 @@
+$(document).ready( function () {
+
+    var lesson = "equations";
+    var lessonDataPath = "../lessons/" + lesson + "/data.json";
+
+    window.iconMap = {
+        video: "icon-facetime-video",
+        exploration: "icon-plane",
+        challenge: "icon-bolt",
+        practice: "icon-ok",
+        quiz: "icon-list-alt",
+        glue: "icon-align-left",
+        text: "icon-align-left"
+    };
+
+    // register helpers
+    Handlebars.registerHelper("equal", function (lvalue, rvalue, options) {
+        if (arguments.length < 3) {
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+        }
+        if ( lvalue !== rvalue ) {
+            return options.inverse(this);
+        } else {
+            return options.fn(this);
+        }
+    });
+
+    Handlebars.registerHelper('eachIndexed', function (context, options) {
+        var fn = options.fn, inverse = options.inverse;
+        var ret = "";
+
+        if (context && context.length > 0) {
+            for (var i=0, j=context.length; i<j; i++) {
+                context[i]["index"] = i+1;
+                ret = ret + fn(context[i]);
+            }
+        } else {
+            ret = inverse(this);
+        }
+        return ret;
+    });
+
+    // register partials
+    Handlebars.registerPartial("video-node", $("#video-node-partial").html());
+    Handlebars.registerPartial("practice-node", $("#practice-node-partial").html());
+    Handlebars.registerPartial("challenge-node", $("#challenge-node-partial").html());
+    Handlebars.registerPartial("exploration-node", $("#exploration-node-partial").html());
+    Handlebars.registerPartial("quiz-node", $("#quiz-node-partial").html());
+    Handlebars.registerPartial("multiple-choice", $("#multiple-choice-partial").html());
+    Handlebars.registerPartial("glue-node", $("#glue-node-partial").html());
+    Handlebars.registerPartial("richtext-node", $("#text-node-partial").html());
+
+    // compile templates
+    window.sidebarTemplate = Handlebars.compile($("#sidebar-template").html());
+    window.mainTemplate = Handlebars.compile($("#main-template").html());
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: lessonDataPath,
+        success: renderPage
+    });
+
+    function renderPage (data) {
+
+        _.each(data.modules, function (module, i) {
+            // preprocess data (add nodes and icons)
+            module.nodes = _.map(module.nodes, function (nodeID, i) {
+                var node = data.nodeList[nodeID];
+                var type = node.nodeType;
+                node.icon = iconMap[type];
+                return node;
+            });
+        });
+
+        // store elements in variables
+        var $sidebar = $("#sidebar");
+        var $main = $("#main");
+        var $extra = $("#extra");
+
+        // render templates
+        $sidebar.html(sidebarTemplate(data));
+        $main.append(mainTemplate(data));
+
+        var mainClassCollapsed = "offset3"; // span8
+        var extraClassCollapsed = "span1 offset11 collapsed";
+
+        var mainClassExpanded = ""; // span8
+        var extraClassExpanded = "span4 offset8 expanded";
+
+        var sidebarClassCollapsed = "span1";
+        var sidebarClassExpanded = "span3";
+
+        collapseExtra(0);
+
+        var speed = 400;
+        $extra.toggle(function () {
+            expandExtra(speed);
+        }, function () {
+            collapseExtra(speed);
+        });
+
+        function expandExtra (speed) {
+            $main.switchClass(mainClassCollapsed, mainClassExpanded, speed);
+            $extra.switchClass(extraClassCollapsed, extraClassExpanded, speed);
+        }
+
+        function collapseExtra (speed) {
+            $main.switchClass(mainClassExpanded, mainClassCollapsed, speed);
+            $extra.switchClass(extraClassExpanded, extraClassCollapsed, speed);
+        }
+
+        collapseSidebar(0);
+
+        $sidebar.toggle(function () {
+            expandSidebar(speed);
+        }, function () {
+            collapseSidebar(speed);
+        });
+
+        function expandSidebar (speed) {
+            $sidebar.switchClass(sidebarClassCollapsed, sidebarClassExpanded, speed);
+        }
+
+        function collapseSidebar (speed) {
+            $sidebar.switchClass(sidebarClassExpanded, sidebarClassCollapsed, speed);
+        }
+    }
+
+});
